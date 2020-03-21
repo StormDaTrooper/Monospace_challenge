b5508c297d1ad53db55cff7c3ed4248019ced3de
Evangelos <vpaparrizos@steficon.gr>
2020-03-21 15:24:04 +0200

    Adds subscription creationg test

---
+++ tests/Feature/SubscriptionsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SubscriptionsTest extends TestCase
+{
+    /**
+     * @test
+     */
+
+    public function a_subscription_can_be_create()
+    {
+
+    }
+
+	/**
+	 * @test
+	 */
+    public function the_api_endpoint_should_fetch_data()
+    {
+
+    }
+
+	/**
+	 * @test
+	 */
+	public function a_user_id_is_required()
+	{
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function a_subscription_type_id_is_required()
+	{
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function a_user_must_exist()
+	{
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function a_user_must_be_active()
+	{
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function a_subscription_type_must_exist()
+	{
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function subscriptions_type_can_not_have_active_subscription()
+	{
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function subscriptions_of_another_type_must_exist_to_receive_discount()
+	{
+
+	}
+}
