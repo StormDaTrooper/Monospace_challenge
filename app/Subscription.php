7a166b9193aa9b7c1bb89c6c51c1c2ada1cd6513
Evangelos <vpaparrizos@steficon.gr>
2020-03-21 16:12:44 +0200

    Adds relation of Subscription with Subscription types

--- app/Subscription.php
+++ app/Subscription.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function getSubscriptionTypeName()
+    {
+    	return ($this->subscriptionType()->first())->name;
+    }
+
+	public function subscriptionType()
+	{
+		return $this->belongsTo(SubscriptionType::class);
+	}
 }
